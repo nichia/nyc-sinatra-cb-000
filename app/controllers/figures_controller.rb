fb46f3a246fb2399f5d98a5dc4a0d87196ca804f
nichia <nichia@gmail.com>
2018-10-04 10:00:15 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,116 @@
 class FiguresController < ApplicationController
   # add controller methods
+  # Index page
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'/figures/index'
+  end
+
+  # New figure page
+  get '/figures/new' do
+    @titles = Title.all.sort_by do |title|
+      title.name
+    end
+    @landmarks = Landmark.all.sort_by do |landmark|
+      landmark.name
+    end
+
+    erb :'/figures/new'
+  end
+
+  # Show figure detail page
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  # Post: Create figure
+  post '/figures' do
+    #binding.pry
+    if !Figure.find_by(name: params[:figure][:name])
+      figure = Figure.create(params[:figure])
+    end
+
+    # Not necessary... already updated in Figure.create
+    #if params[:figure][:title_ids]
+    #  figure.titles = params[:figure][:title_ids].collect {|id| Title.find(id)}
+    #end
+
+    if !params[:title][:name].empty?
+      # create title
+      if !Title.find_by(name: params[:title][:name])
+        figure.titles << Title.create(params[:title])
+      else
+        figure.titles << Title.find_by(name: params[:title][:name])
+      end
+    end
+
+    # Not necessary... already updated in Figure.create
+    #if params[:figure][:landmark_ids]
+    #  figure.landmarks = params[:figure][:landmark_ids].collect {|id| Landmark.find(id)}
+    #end
+    if !params[:landmark][:name].empty?
+      # create landmark
+      if !Landmark.find_by(name: params[:landmark][:name])
+        figure.landmarks << Landmark.create(params[:landmark])
+      else
+        figure.landmarks << Landmark.find_by(name: params[:landmark][:name])
+      end
+    end
+
+    #figure.titles.build(params["title"]) unless params["title"]["name"].empty?
+    #figure.landmarks.build(params["landmark"]) unless params["landmark"]["name"].empty?
+
+    figure.save
+
+    redirect :"/figures/#{figure.id}"
+  end
+
+  # Edit a figure
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+
+    @titles = Title.all.sort_by do |title|
+      title.name
+    end
+    @landmarks = Landmark.all.sort_by do |landmark|
+      landmark.name
+    end
+
+    erb :'/figures/edit'
+  end
+
+  # Patch: Update figure
+  patch '/figures/:id' do
+    #binding.pry
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+
+    #figure.titles.build(params["title"]) unless params["title"]["name"].empty?
+    if !params[:title][:name].empty?
+      # create title
+      if !Title.find_by(name: params[:title][:name])
+        figure.titles << Title.create(params[:title])
+      else
+        figure.titles << Title.find_by(name: params[:title][:name])
+      end
+    end
+
+    #figure.landmarks.build(params["landmark"]) unless params["landmark"]["name"].empty?
+    if !params[:landmark][:name].empty?
+      # create landmark
+      if !Landmark.find_by(name: params[:landmark][:name])
+        figure.landmarks << Landmark.create(params[:landmark])
+      else
+        figure.landmarks << Landmark.find_by(name: params[:landmark][:name])
+      end
+    end
+
+    figure.save
+
+    redirect :"/figures/#{figure.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,54 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  # Index page
+  get '/landmarks' do
+    @landmarks = Landmark.all
+
+    erb :'/landmarks/index'
+  end
+
+  # New landmark page
+  get '/landmarks/new' do
+
+    erb :'/landmarks/new'
+  end
+
+  # Show landmark detail page
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  # Post: Create landmark
+  post '/landmarks' do
+    #binding.pry
+    if !Landmark.find_by(name: params[:landmark][:name])
+      landmark = Landmark.create(params[:landmark])
+    end
+
+    redirect :"/landmarks/#{landmark.id}"
+  end
+
+  # Edit a landmark
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+
+    erb :'/landmarks/edit'
+  end
+
+  # Patch: Update landmark
+  patch '/landmarks/:id' do
+    #binding.pry
+    landmark = Landmark.find(params[:id])
+
+    landmark.update(params[:landmark])
+    #landmark.name = params[:landmark][:name]
+    #landmark.year_completed = params[:landmark][:year_completed]
+    #landmark.save
+
+    redirect :"/landmarks/#{landmark.id}"
+  end
+
 end
